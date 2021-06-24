fa4ae8a06cfc2dc695294a259ade832d97c622ad
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-25 08:24:51 +1000

    Comment change

--- src/services/festivalDataTransformer.js
+++ src/services/festivalDataTransformer.js
@@ -1,6 +1,5 @@
-//This function takes festival data from https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals
-//which is organised by festival names with band data under each festival and converts it to a list of
-//record labels with band name and festivals beneath.
+//Convert festival data to requested format 
+
 const { expectedResponse } = require('../services/getFestivals');
 const festivals = expectedResponse; 
 const {containsRecordLabel} = require('../services/dataCheckers/containsRecordLabel');
