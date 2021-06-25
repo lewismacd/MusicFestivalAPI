c15569528f5f9855a9035a7a45222336809e59ad
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-25 15:16:03 +1000

    Fixed bug discovered during test writing

--- src/services/dataCheckers/containsFestival.js
+++ src/services/dataCheckers/containsFestival.js
@@ -3,7 +3,7 @@ function containsFestival(recordLabels, recordLabel, band, festival){
     for (let label of recordLabels) {
         if (label.label == recordLabel){
             for(let i of label.bands){
-                if (i.name == band.name){                  
+                if (i.name == band){                 
                     for (let j of i.festivals){
                         if (j.name == festival){
                             return true
