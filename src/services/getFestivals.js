6c2c0c8b52a4fc8e26b247e41898e8026d3d615d
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-28 09:52:10 +1000

    Add environment variable to serverless.yml and use as URL in getFestivals function

--- src/services/getFestivals.js
+++ src/services/getFestivals.js
@@ -2,9 +2,10 @@ const { RESTDataSource } = require("apollo-datasource-rest");
 class FestivalDataSource extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://eacp.energyaustralia.com.au/codingtest';
+    this.baseURL = process.env.FESTIVAL_ENDPOINT;
   }
   async getFestivals() {
+    console.log(this.baseURL);
     return this.get(`/api/v1/festivals`);
   }
 }
