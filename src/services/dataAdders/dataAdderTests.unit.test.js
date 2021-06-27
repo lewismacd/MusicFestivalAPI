37af477736c8de4a84d16834e7076b119bd7d04f
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-27 18:36:50 +1000

    Update data Adder Tests

--- src/services/dataAdders/dataAdderTests.unit.test.js
+++ src/services/dataAdders/dataAdderTests.unit.test.js
@@ -1,22 +1,64 @@
 //Tests to check that adding a new record/band/festival occurs correctly.
 const {addNewRecordLabel, addBandToExistingLabel, addFestivalToExistingBand} = require('./dataAdders.js');
 
+const existingRecordLabel = "Record Label 2";
 const sampleFestival1 = "Sample Festival 1";
-
+const sampleFestival2 = "Sample Festival 2";
 const sampleBand1 = 
 {
     "name": "Sample Band 1",
     "recordLabel": "Sample Record Label 1"
 }
+const sampleBand2 = 
+{
+    "name": "Sample Band 2",
+    "recordLabel": "Record Label 2"
+}
 
-//Check adding new record occurs as expected using sample data
-test("Check adding new record occurs as expected, formatted correctly", () => {
-    addNewRecordLabel(sampleNewData, sampleBand1, sampleFestival1);
-    expect(sampleNewData).toEqual(addNewRecordExpectedResult);
-    //Reset data???
-});
+//Sample data for Record Adder Test
+const sampleNewDataRecordAdder =
+[
+    {
+        "label": "Record Label 1",
+        "bands": [
+            {
+                "name": "Band 1",
+                "festivals": [
+                    {
+                        "name": "Festival 1",
+                    }
+                ]
+            },
+            {
+                "name": "Band 2",
+                "festivals": [
+                    {
+                        "name": "Festival 2",
+                    },
+                    {
+                        "name": "Festival 3",
+                    }
 
-const addNewRecordExpectedResult = 
+                ]
+            }
+        ]
+    },
+    {
+        "label": "Record Label 2",
+        "bands": [
+            {
+                "name": "Band 3",
+                "festivals": [
+                    {
+                        "name": "Festival 1"
+                    }
+                ]
+            }
+        ]
+    }
+]
+//Expected result after adding new record
+const addNewRecordTestExpectedResult = 
 [
     {
         "label": "Record Label 1",
@@ -70,103 +112,161 @@ const addNewRecordExpectedResult =
         ]
     }
 ]
+//Sample data for Band Adder Test
+const sampleNewDataBandAdder =
+[
+    {
+        "label": "Record Label 1",
+        "bands": [
+            {
+                "name": "Band 1",
+                "festivals": [
+                    {
+                        "name": "Festival 1",
+                    }
+                ]
+            },
+            {
+                "name": "Band 2",
+                "festivals": [
+                    {
+                        "name": "Festival 2",
+                    },
+                    {
+                        "name": "Festival 3",
+                    }
+
+                ]
+            }
+        ]
+    },
+    {
+        "label": "Record Label 2",
+        "bands": [
+            {
+                "name": "Band 3",
+                "festivals": [
+                    {
+                        "name": "Festival 1"
+                    }
+                ]
+            }
+        ]
+    }
+]
+//Expected result after adding new band
+const addNewBandTestExpectedResult = 
+[
+    {
+        "label": "Record Label 1",
+        "bands": [
+            {
+                "name": "Band 1",
+                "festivals": [
+                    {
+                        "name": "Festival 1",
+                    }
+                ]
+            },
+            {
+                "name": "Band 2",
+                "festivals": [
+                    {
+                        "name": "Festival 2",
+                    },
+                    {
+                        "name": "Festival 3",
+                    }
+
+                ]
+            }
+        ]
+    },
+    {
+        "label": "Record Label 2",
+        "bands": [
+            {
+                "name": "Band 3",
+                "festivals": [
+                    {
+                        "name": "Festival 1"
+                    }
+                ]
+            },
+            {
+                "name": "Sample Band 2",
+                "festivals": [
+                    {
+                        "name": "Sample Festival 2"
+                    }
+                ]
+            }       
+        ]
+    }
+]
+
+//Sample data of a partially complete transformation
+const sampleNewDataFestivalAdder = 
+[
+    {
+        "label": "Record Label 1",
+        "bands": [
+            {
+                "name": "Band 1",
+                "festivals": [
+                    {
+                        "name": "Festival 1",
+                    }
+                ]
+            },
+            {
+                "name": "Band 2",
+                "festivals": [
+                    {
+                        "name": "Festival 2",
+                    },
+                    {
+                        "name": "Festival 3",
+                    }
+
+                ]
+            }
+        ]
+    },
+    {
+        "label": "Record Label 2",
+        "bands": [
+            {
+                "name": "Band 3",
+                "festivals": [
+                    {
+                        "name": "Festival 1"
+                    }
+                ]
+            }
+        ]
+    }
+]
+//Expected result after adding new festival
+const addNewFestivalTestExpectedResult = [];
+
+//Check adding new record occurs as expected using sample data
+test("Check adding new record occurs as expected, formatted correctly", () => {
+    addNewRecordLabel(sampleNewDataRecordAdder, sampleBand1, sampleFestival1);
+    expect(sampleNewDataRecordAdder).toEqual(addNewRecordTestExpectedResult);
+});
 
 //Check adding new band happens as expected using sample data
 test("Check adding new band occurs as expected, formatted correctly", () => {
-    //TBC
+    addBandToExistingLabel(sampleNewDataBandAdder, existingRecordLabel, sampleBand2, sampleFestival2);
+    expect(sampleNewDataBandAdder).toEqual(addNewBandTestExpectedResult);
 });
-
-const addNewBandTestExpectedResult = [];
 
 //Check adding new festival to band happens as expected using sample data
 test("Check adding new festival occurs as expected, formatted correctly", () => {
     //TBC
 });
 
-const addNewFestivalTestExpectedResult = [];
 
-//Sample data of a partially complete transformation
-const sampleNewData =
-[
-    {
-        "label": "Record Label 1",
-        "bands": [
-            {
-                "name": "Band 1",
-                "festivals": [
-                    {
-                        "name": "Festival 1",
-                    }
-                ]
-            },
-            {
-                "name": "Band 2",
-                "festivals": [
-                    {
-                        "name": "Festival 2",
-                    },
-                    {
-                        "name": "Festival 3",
-                    }
 
-                ]
-            }
-        ]
-    },
-    {
-        "label": "Record Label 2",
-        "bands": [
-            {
-                "name": "Band 3",
-                "festivals": [
-                    {
-                        "name": "Festival 1"
-                    }
-                ]
-            }
-        ]
-    }
-]
 
-//Reset sample data after each test
-const sampleNewDataReset =
-[
-    {
-        "label": "Record Label 1",
-        "bands": [
-            {
-                "name": "Band 1",
-                "festivals": [
-                    {
-                        "name": "Festival 1",
-                    }
-                ]
-            },
-            {
-                "name": "Band 2",
-                "festivals": [
-                    {
-                        "name": "Festival 2",
-                    },
-                    {
-                        "name": "Festival 3",
-                    }
-
-                ]
-            }
-        ]
-    },
-    {
-        "label": "Record Label 2",
-        "bands": [
-            {
-                "name": "Band 3",
-                "festivals": [
-                    {
-                        "name": "Festival 1"
-                    }
-                ]
-            }
-        ]
-    }
-]
