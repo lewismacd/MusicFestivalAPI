bf5958dd3cc4df6979ac982ac88cc310429668a2
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-27 16:37:53 +1000

    Remove unnecessary code

--- src/services/getFestivals.unit.test.js
+++
@@ -1,9 +0,0 @@
-//Test that getFestivals can successfully fetch data from API
-
-// const nock = require('nock')
-
-// const scope = nock('https://eacp.energyaustralia.com.au/')
-//   .get('/codingtest/api/v1/festivals')
-//   .reply(200, {
-//     //PUT NOCK REPLY HERE!!!
-//   })
