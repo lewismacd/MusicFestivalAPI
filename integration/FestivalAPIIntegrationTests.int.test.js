6e612c9291ce0b32f7c863ee9737ef1b31a89f4c
Lewis MacDonald <lewismacdonald@LewisMacDonald.local>
2021-06-28 05:50:14 +1000

    Got nock working for integration test

--- integration/FestivalAPIIntegrationTests.int.test.js
+++ integration/FestivalAPIIntegrationTests.int.test.js
@@ -5,11 +5,85 @@ const resolvers = require('../src/resolvers/recordLabelResolver');
 const {FestivalDataSource} = require('../src/services/getFestivals.js');
 const nock = require('nock')
 
+const sampleFestivalData = 
+[
+    {
+      "name": "Festival 1",
+      "bands": [
+        {
+          "name": "Band 1",
+          "recordLabel": "Record Label 1"
+        },
+        {
+          "name": "Band 2",
+          "recordLabel": "Record Label 2"
+        }
+      ]
+    },
+    {
+      "name": "Festival 2",
+      "bands": [
+        {
+          "name": "Band 3",
+          "recordLabel": "Record Label 3"
+        },
+        {
+          "name": "Band 1",
+          "recordLabel": "Record Label 1"
+        }
+      ]
+    }
+]
+const expectedLabelData = 
+[
+    {
+      "label": "Record Label 1",
+      "bands": [
+        {
+          "name": "Band 1",
+          "festivals": [
+              {
+                  "name": "Festival 1",
+              },
+              {
+                   "name": "Festival 2"
+              }
+            ]
+        },
+      ]
+    },
+    {
+      "label": "Record Label 2",
+      "bands": [
+        {
+          "name": "Band 2",
+          "festivals": [
+            {
+                "name": "Festival 1",
+            }
+          ]
+        }
+      ]
+    },
+    {
+        "label": "Record Label 3",
+        "bands": [
+          {
+            "name": "Band 3",
+            "festivals": [
+              {
+                  "name": "Festival 2",
+              }
+            ]
+          }
+        ]
+      }
+];
+
+//Use nock to intercept and mock API response
 const scope = nock('https://eacp.energyaustralia.com.au/')
   .get('/codingtest/api/v1/festivals')
-  .reply(200, {
-    //PUT NOCK REPLY HERE!!!
-  })
+  .reply(200, sampleFestivalData)
 
 const server = new ApolloServer({
     typeDefs,
@@ -39,10 +113,8 @@ test('find record labels', async () => {
   `;
 
   const {
-    data: { labels },
+    data: { recordLabels },
   } = await query({ query: RETURN_LABELS });
 
-  expect(labels).toEqual({
-    //PUT EXPECTED STUFF HERE
-  });
+  expect(recordLabels).toEqual(expectedLabelData);
 });
